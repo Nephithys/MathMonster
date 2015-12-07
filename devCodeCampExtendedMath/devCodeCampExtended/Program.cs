1cfb44fdec5716335d201e09ff44e4783363006c
Nephithys <Nephithys@gmail.com>
2015-12-07 17:29:59 -0600

    first draft

---
+++ devCodeCampExtendedMath/devCodeCampExtended/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devCodeCampExtendedMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            List<int> happynums = new List<int>();
+
+            while (happynums.Count < 8)
+            {
+                if (ishappy(num))
+                {
+                    happynums.Add(num);
+                }
+                num++;
+            }
+            Console.WriteLine("First 8 happy numbers : " + string.Join(",", happynums));
+        }
+    }
+    
+}
---
+++ devCodeCampExtendedMath/devCodeCampExtendedMath/math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devCodeCampExtendedMath
+{
+    public static class Math
+    {
+        public static bool ishappy(int n)
+        {
+            List<int> cache = new List<int>();
+            int sum = 0;
+            while (n != 1)
+            {
+                if (cache.Contains(n))
+                {
+                    return false;
+                }
+                cache.Add(n);
+                while (n != 0)
+                {
+                    int digit = n % 10;
+                    sum += digit * digit;
+                    n /= 10;
+                }
+                n = sum;
+                sum = 0;
+            }
+            return true;
+        }
+    }
+}
