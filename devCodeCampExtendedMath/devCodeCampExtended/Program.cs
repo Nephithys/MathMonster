c0809c1310b25a44841ac1d7166994fc2ff6add9
Nephithys <Nephithys@gmail.com>
2015-12-08 10:39:56 -0600

    added EuclMath class and romenums class

--- devCodeCampExtendedMath/devCodeCampExtended/Program.cs
+++ devCodeCampExtendedMath/devCodeCampExtended/Program.cs
@@ -3,17 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using devCodeCampExtendedMath;
 
 namespace devCodeCampExtendedMath
 {
     class Program
     {
+        public static bool ishappy(int n)
+        {
+            List<int> cache = new List<int>();
+            int sum = 0;
+            while (n != 1)
+            {
+                if (cache.Contains(n))
+                {
+                    return false;
+                }
+                cache.Add(n);
+                while (n != 0)
+                {
+                    int digit = n % 10;
+                    sum += digit * digit;
+                    n /= 10;
+                }
+                n = sum;
+                sum = 0;
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
             int num = 1;
             List<int> happynums = new List<int>();
 
-            while (happynums.Count < 8)
+            while (happynums.Count < 1000)
             {
                 if (ishappy(num))
                 {
@@ -21,7 +44,8 @@ namespace devCodeCampExtendedMath
                 }
                 num++;
             }
-            Console.WriteLine("First 8 happy numbers : " + string.Join(",", happynums));
+            Console.WriteLine("First 1000 happy numbers : " + string.Join(",", happynums));
+            Console.WriteLine($"the 50th num is { happynums[50]}");
         }
     }
     
--- devCodeCampExtendedMath/devCodeCampExtendedMath/math.cs
+++ devCodeCampExtendedMath/devCodeCampExtendedMath/math.cs
@@ -6,29 +6,36 @@ using System.Threading.Tasks;
 
 namespace devCodeCampExtendedMath
 {
-    public static class Math
+    public static class MathRome
     {
-        public static bool ishappy(int n)
+        public static string ToRomanNumeral(this int value)
         {
-            List<int> cache = new List<int>();
-            int sum = 0;
-            while (n != 1)
+            if (value < 0)
+                throw new ArgumentOutOfRangeException("Please use a positive integer greater than zero.");
+
+            StringBuilder sb = new StringBuilder();
+            int remain = value;
+            while (remain > 0)
             {
-                if (cache.Contains(n))
-                {
-                    return false;
-                }
-                cache.Add(n);
-                while (n != 0)
-                {
-                    int digit = n % 10;
-                    sum += digit * digit;
-                    n /= 10;
-                }
-                n = sum;
-                sum = 0;
+                if (remain >= 1000) { sb.Append("M"); remain -= 1000; }
+                else if (remain >= 900) { sb.Append("CM"); remain -= 900; }
+                else if (remain >= 500) { sb.Append("D"); remain -= 500; }
+                else if (remain >= 400) { sb.Append("CD"); remain -= 400; }
+                else if (remain >= 100) { sb.Append("C"); remain -= 100; }
+                else if (remain >= 90) { sb.Append("XC"); remain -= 90; }
+                else if (remain >= 50) { sb.Append("L"); remain -= 50; }
+                else if (remain >= 40) { sb.Append("XL"); remain -= 40; }
+                else if (remain >= 10) { sb.Append("X"); remain -= 10; }
+                else if (remain >= 9) { sb.Append("IX"); remain -= 9; }
+                else if (remain >= 5) { sb.Append("V"); remain -= 5; }
+                else if (remain >= 4) { sb.Append("IV"); remain -= 4; }
+                else if (remain >= 1) { sb.Append("I"); remain -= 1; }
+                else throw new Exception("Unexpected error."); // <<-- shouldn't be possble to get here, but it ensures that we will never have an infinite loop (in case the computer is on crack that day).
             }
-            return true;
+
+            return sb.ToString();
         }
+
+       
     }
 }
